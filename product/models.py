c79d5c607fca3174c324bbd4959de1259d529034
OrlandoFiallos <contactofiallosorlando@gmail.com>
2023-02-24 19:18:42 -0600

    Agregando campo slug a category

--- product/models.py
+++ product/models.py
@@ -6,13 +6,18 @@ from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     
     class Meta:
         ordering = ('name',)
     
     def __str__(self) -> str:
         return self.name 
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
     
     def get_absolute_url(self):
         return f'/{self.slug}/'
